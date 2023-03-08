48686a39b1f02a2d9c5fe2052c9eef594eff79fa
nimiCurtis <nimicu21@gmail.com>
2023-03-08 14:18:49 +0200

    update dev

---
+++ src/data/dataset.py
@@ -0,0 +1,30 @@
+import numpy as npy 
+import cv2
+import pandas as pd 
+from bag_utils.modules.bag_parser.bag_parser import Parser
+from bag_utils.modules.bag_reader.bag_reader import BagReader
+
+# take a batch of bags iterate on the bag files:
+# - take the set and label params from the config
+# - load the df from the metadata 
+# - load the npy files and save them in the right set in right label as images
+
+
+def split_to_train_test():
+    
+    
+    
+    pass
+
+
+
+
+
+
+def main():
+    pass
+
+
+
+if __name__=='__main__':
+    main()
